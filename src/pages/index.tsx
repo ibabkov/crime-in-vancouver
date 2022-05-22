7c81b525b21181b18f78840e4b678acbf05e68ca
ibabkov <iv.babkov@gmail.com>
2022-05-22 15:58:53 +0300

    edit: react-map-gl version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { ApplicationContainer } from '../containers/Application';
 // import {DSVParsedArray, DSVRowString} from "d3-dsv";
 
 const MainPage: NextPage = (props: any) => {
-  return <ApplicationContainer data={props.data} />;
+  return <ApplicationContainer data={mapData(props.data)} />;
 };
 
 async function getFetchData(): Promise<any> {
@@ -15,37 +15,37 @@ async function getFetchData(): Promise<any> {
       'https://crime-in-vancouver-ibabkov.vercel.app/data.csv',
       (error, response) => {
         if (!error) {
-          // resolve(response);
-          resolve({
-            type: 'FeatureCollection',
-            features: response.map(({ lat, lon }, i) => {
-              delete response[i];
-
-              return {
-                type: 'Feature',
-                geometry: { type: 'Point', coordinates: [lon, lat] },
-              } as unknown as Feature<Point>;
-            }),
-          });
+          resolve(response);
+          // resolve({
+          //   type: 'FeatureCollection',
+          //   features: response.map(({ lat, lon }, i) => {
+          //     delete response[i];
+          //
+          //     return {
+          //       type: 'Feature',
+          //       geometry: { type: 'Point', coordinates: [lon, lat] },
+          //     } as unknown as Feature<Point>;
+          //   }),
+          // });
         }
       }
     );
   });
 }
 
-// function mapData(array: any): Promise<FeatureCollection<Point>> {
-//   return resolve({
-//     type: 'FeatureCollection',
-//     features: response.map(({ lat, lon }, i) => {
-//       delete response[i];
-//
-//       return {
-//         type: 'Feature',
-//         geometry: { type: 'Point', coordinates: [lon, lat] },
-//       } as unknown as Feature<Point>;
-//     }),
-//   });
-// }
+function mapData(array: any): any {
+  return {
+    type: 'FeatureCollection',
+    features: array.map(({ lat, lon }: any, i: number) => {
+      delete array[i];
+
+      return {
+        type: 'Feature',
+        geometry: { type: 'Point', coordinates: [lon, lat] },
+      } as unknown as Feature<Point>;
+    }),
+  };
+}
 
 MainPage.getInitialProps = async (ctx) => {
   const data = await getFetchData();
